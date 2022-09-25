6cb7ef7d17997cfbdeb6677ab4721117fea21c03
Sora <sora.x@hotmail.fr>
2022-09-25 17:19:40 +0200

    Hud: add dreamstone counter + Hud Plus support

--- SADX-Klonoa-Gameplay/hud.cpp
+++ SADX-Klonoa-Gameplay/hud.cpp
@@ -4,13 +4,14 @@
 static NJS_TEXNAME KHud[4] = { 0 };
 static NJS_TEXLIST KHudTexlist = { arrayptrandlength(KHud) };
 static const float ringPos = 52.0f;
+static bool HudPlus = false;
 
 static NJS_TEXANIM Hud_TEXANIM[] = {
 	{ 0x20, 0x27, 0, 0, 0, 0, 0x100, 0x100, dream, 0x20 },
 	{ 0x32, 0x27, 0, 0, 0, 0, 0x100, 0x100, heartEmpty, 0x20 },
 	{ 0x32, 0x27, 0, 0, 0, 0, 0x100, 0x100, heart, 0x20 },
 	{ 230, 96, 0, 0, 0, 0, 0x100, 0x100, bgHeart, 0x20 },
-	{ 0x52, 0x30, 0, 0, 0, 0, 0x100, 0x100, bgLifeStone, 0x20 },
+	{ 0x68, 0x3B, 0, 0, 0, 0, 0x100, 0x100, bgLifeStone, 0x20 },
 };
 
 NJS_SPRITE Heart_SPRITE = { {0}, 1.0f, 1.0f, {-1000}, &KHudTexlist, Hud_TEXANIM };
@@ -30,14 +31,14 @@ bool isHudAllowed()
 
 static Uint32 StoneTimer = 0;
 
-void DrawDreamStoneCounter(bool up) {
+void DrawDreamStoneCounter(float posY, bool boss) {
 
 	auto backup = Hud_RingTimeLife;
 	Hud_RingTimeLife.ang = -1000;
 
-	Hud_RingTimeLife.p.x = HorizontalStretch * 520.0f;
-	Hud_RingTimeLife.p.y = ringPos - 10.0f + up ? 40.0f : 0.0f;
-
+	Hud_RingTimeLife.p.x = HorizontalStretch * 583.0f;
+	Hud_RingTimeLife.p.y = ringPos - 12.0f + posY;
+	Hud_RingTimeLife.p.y += boss ? 59.5f : 0.0f;
 
 	int16_t rings = Rings;
 
@@ -53,8 +54,9 @@ void DrawDreamStoneCounter(bool up) {
 		SetMaterialAndSpriteColor_Float(1, 1, color, color);
 
 		for (uint8_t i = 0; i < 3; ++i) {
-			njDrawSprite2D_ForcePriority(&Hud_RingTimeLife, 0, -1.501f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR | NJD_SPRITE_ANGLE);
+			njDrawSprite2D_ForcePriority(&Hud_RingTimeLife, dream, -1.501f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR | NJD_SPRITE_ANGLE);
 			Hud_RingTimeLife.p.x -= 16;
+			Hud_RingTimeLife.p.y += 2.0f;
 		}
 	}
 	else {
@@ -69,38 +71,43 @@ void DrawDreamStoneCounter(bool up) {
 			else {
 				njDrawSprite2D_ForcePriority(&Hud_RingTimeLife, rings % 10, -1.501f, NJD_SPRITE_ALPHA | NJD_SPRITE_ANGLE);
 				rings /= 10;
+
 			}
 
 			Hud_RingTimeLife.p.x -= 16;
+			Hud_RingTimeLife.p.y += 2.0f;
 		}
 	}
 
 	Hud_RingTimeLife = backup;
 }
 
-void DrawDreamStoneHUD()
+void DrawDreamStoneHUD(bool bosslevel)
 {
-	float posY = isBossLevel() ? 40.0f : 0.0f;
+	//isBossLevel() ? 40.0f : 0.0f;
+	float posX = HorizontalStretch * 520.0f;
+	static const float posY = HudPlus ? 0.0f : 20.0f;
 
-	float posX = HorizontalStretch * 520.0;
 	Hud_BGSprite.p.x = posX;
-	Hud_BGSprite.p.y = ringPos - 10.0f + posY;
+	Hud_BGSprite.p.y = ringPos - posY - bosslevel ? 90.0f : 0.0f;
 	Stone_SPRITE.p = Hud_BGSprite.p;
 
-	SetMaterialAndSpriteColor_Float(1.0f, 0.086, 0.890, 0.129); //set green color to dream stone icon
+	SetMaterialAndSpriteColor_Float(1.0f, 0.086f, 0.890f, 0.129f); //set green color to dream stone icon
 	late_DrawSprite2D(&Stone_SPRITE, dream, 22045.498f, NJD_SPRITE_ANGLE | NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR, LATE_LIG);
 
-	SetMaterialAndSpriteColor_Float(1.0f, 0.129, 0.576, 0.709); //set blue color
+	Hud_BGSprite.p.y -= 10.0f;
+	SetMaterialAndSpriteColor_Float(1.0f, 0.129f, 0.576f, 0.709f); //set blue color
 	late_DrawSprite2D(&Hud_BGSprite, bgLifeStone, 22044.498f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR, LATE_LIG);
 	ResetMaterial();
 }
 
 void DrawHeartHUD()
 {
+	static const float y = HudPlus ? 0.0f : 12.0f;
 	Heart_SPRITE.p.x = 10;
-	Heart_SPRITE.p.y = ringPos + 50.0f;
+	Heart_SPRITE.p.y = ringPos + 50.0f -y;
 	Hud_BGSprite.p.x = -5;
-	Hud_BGSprite.p.y = ringPos + 13.0f;
+	Hud_BGSprite.p.y = ringPos + 13.0f -y;
 
 	late_DrawSprite2D(&Hud_BGSprite, bgHeart, 22045.498f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR, LATE_LIG);
 }
@@ -123,8 +130,10 @@ void DrawKlonoaHUD()
 		DrawKlonoaHP();
 	}
 
-	DrawDreamStoneHUD();
-	DrawDreamStoneCounter(isBossLevel());
+	bool boss = isBossLevel();
+	DrawDreamStoneHUD(boss);
+	float posY = HudPlus ? 20.0f : 0.0f;
+	DrawDreamStoneCounter(posY, boss);
 
 	njColorBlendingMode(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
 	njColorBlendingMode(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_ONE);
@@ -141,8 +150,10 @@ void loadKLHudTex()
 	LoadPVM("hud", &KHudTexlist);
 }
 
-
 void init_Hud()
 {
-
+	if (hud) {
+		HMODULE hud = GetModuleHandle(L"sadx-hud-plus");
+		HudPlus = hud != NULL;
+	}
 }
